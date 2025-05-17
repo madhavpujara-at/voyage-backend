53ffeb60212618e3017961c60ca973ed08c95df0
Madhav Pujara <madhav.pujara@avestatechnologies.com>
2025-05-17 19:05:26 +0530

    chore: Remove dist folder contents and update .gitignore to exclude dist directory

--- dist/modules/auth/domain/entities/User.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// This file mostly serves as a pass-through to the Prisma-generated User type
-// If we need to add domain-specific logic to the User entity, we can extend it here
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRole = void 0;
-const prisma_1 = require("../../../../infrastructure/database/generated/prisma");
-Object.defineProperty(exports, "UserRole", { enumerable: true, get: function () { return prisma_1.UserRole; } });
-//# sourceMappingURL=User.js.map
